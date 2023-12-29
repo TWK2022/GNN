53cf51d1ed49c5c623e71783b8c559818d85468c
TWK <109967047+TWK2022@users.noreply.github.com>
2023-12-29 17:24:17 +0800

    !

--- run.py
+++ run.py
@@ -3,11 +3,9 @@ import wandb
 import torch
 import argparse
 from block.data_get import data_get
+from block.loss_get import loss_get
 from block.model_get import model_get
 from block.train_get import train_get
-
-# -------------------------------------------------------------------------------------------------------------------- #
-# 数据格式：
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # 分布式训练：
@@ -17,34 +15,33 @@ from block.train_get import train_get
 # -------------------------------------------------------------------------------------------------------------------- #
 # 设置
 parser = argparse.ArgumentParser(description='|图神经网络|')
-parser.add_argument('--data_path', default=r'', type=str, help='|数据路径|')
-parser.add_argument('--weight', default='last.pt', type=str,
-                    help='|会优先加载之前训练时保存的.pt模型继续训练，没有则新建peft再训练|')
-parser.add_argument('--save_pt', default=5, type=int,
-                    help='|每几轮保存一次完整的last.pt模型以便中断后继续训练，0为不保存|')
+parser.add_argument('--data_path', default=r'./dataset/200data.pt', type=str, help='|数据位置|')
+parser.add_argument('--divide', default='9,1', type=str, help='|训练集和验证集划分比例|')
+parser.add_argument('--weight', default='last.pt', type=str, help='|已有模型的位置，如果没找到模型则会创建新模型|')
+parser.add_argument('--save_path', default='best.pt', type=str, help='|保存最佳模型，除此之外每轮还会保存last.pt|')
 parser.add_argument('--wandb', default=False, type=bool, help='|是否使用wandb可视化|')
-parser.add_argument('--wandb_project', default='GPT', type=str, help='|wandb项目名称|')
+parser.add_argument('--wandb_project', default='TimeSeriesForecasting', type=str, help='|wandb项目名称|')
 parser.add_argument('--wandb_name', default='train', type=str, help='|wandb项目中的训练名称|')
-parser.add_argument('--epoch', default=25, type=int, help='|训练轮数|')
-parser.add_argument('--batch', default=2, type=int, help='|训练批量大小|')
-parser.add_argument('--lr_start', default=0.00001, type=float,
-                    help='|初始学习率，训练中采用adam算法，前3轮有预热训练，基准为0.00001|')
+parser.add_argument('--model', default='linear_conv', type=str, help='|自定义模型选择|')
+parser.add_argument('--model_type', default='m', type=str, help='|自定义模型型号|')
+parser.add_argument('--epoch', default=200, type=int, help='|训练轮数|')
+parser.add_argument('--batch', default=64, type=int, help='|训练批量大小|')
+parser.add_argument('--loss', default='mse_decay', type=str, help='|损失函数|')
+parser.add_argument('--lr_start', default=0.001, type=float, help='|初始学习率，adam算法，3轮预热训练，基准为0.001|')
 parser.add_argument('--lr_end_ratio', default=0.2, type=float, help='|最终学习率=lr_end_ratio*lr_start，基准为0.2|')
-parser.add_argument('--lr_adjust_num', default=20, type=int,
-                    help='|从初始学习率到最终学习率经过的调整次数，余玄下降法，一般小于总轮次|')
-parser.add_argument('--lr_adjust_threshold', default=0.97, type=float,
-                    help='|本轮训练损失大于上一轮损失的比例时才调整，基准为0.97|')
+parser.add_argument('--lr_adjust_num', default=100, type=int, help='|学习率下降调整次数，余玄下降法，要小于总轮次|')
+parser.add_argument('--lr_adjust_threshold', default=0.97, type=float, help='|本轮损失下降一定比例时才调整，基准为0.97|')
 parser.add_argument('--regularization', default='L2', type=str, help='|正则化，有L2、None|')
 parser.add_argument('--r_value', default=0.0005, type=float, help='|正则化的权重系数|')
 parser.add_argument('--device', default='cuda', type=str, help='|训练设备|')
 parser.add_argument('--latch', default=True, type=bool, help='|模型和数据是否为锁存，True为锁存|')
-parser.add_argument('--num_worker', default=0, type=int,
-                    help='|CPU在处理数据时使用的进程数，0表示只有一个主进程，一般为0、2、4、8|')
+parser.add_argument('--num_worker', default=0, type=int, help='|CPU处理数据的进程数，0表示只有一个主进程，一般为0、2、4、8|')
 parser.add_argument('--ema', default=True, type=bool, help='|使用平均指数移动(EMA)调整参数|')
-parser.add_argument('--amp', default=False, type=bool, help='|混合float16精度训练，CPU时不可用|')
+parser.add_argument('--amp', default=True, type=bool, help='|混合float16精度训练，CPU时不可用|')
 parser.add_argument('--distributed', default=False, type=bool, help='|单机多卡分布式训练，分布式训练时batch为总batch|')
 parser.add_argument('--local_rank', default=0, type=int, help='|分布式训练使用命令后会自动传入的参数|')
 args = parser.parse_args()
+args.divide = list(map(int, args.divide.split(',')))
 args.device_number = max(torch.cuda.device_count(), 1)  # 使用的GPU数，可能为CPU
 print(f'| args:{args} |')
 # 为CPU设置随机种子
@@ -69,11 +66,13 @@ if args.distributed:
     args.device = torch.device("cuda", args.local_rank)
 # -------------------------------------------------------------------------------------------------------------------- #
 # 初步检查
-assert os.path.exists(args.data_path), f'! data_path不存在:{args.data_path} !'
-if os.path.exists(args.weight):
-    print(f'| 加载模型继续训练:{args.weight} |')
-else:
-    print(f'| 创建peft模型以微调模型:{args.model_path} |')
+if args.local_rank == 0:
+    assert os.path.exists(args.data_path), f'! data_path不存在:{args.data_path} !'
+    if os.path.exists(args.weight):
+        print(f'| 加载已有模型:{args.weight} |')
+    else:
+        assert os.path.exists(f'model/{args.model}.py'), f'! 没有自定义模型:{args.model} !'
+        print(f'| 创建自定义模型:{args.model} | 型号:{args.model_type} |')
 # -------------------------------------------------------------------------------------------------------------------- #
 # 程序
 if __name__ == '__main__':
@@ -81,5 +80,11 @@ if __name__ == '__main__':
     data_dict = data_get(args)
     # 模型
     model_dict = model_get(args)
+    # 损失
+    loss = loss_get(args)
+    # 摘要
+    print('| 训练集:{} | 验证集:{} | 批量{} | 模型:{} | 输入长度:{} | 输出长度:{} | 损失函数:{} | 初始学习率:{} |'
+          .format(len(data_dict['train_input']), len(data_dict['val_input']), args.batch, args.model, args.input_size,
+                  args.output_size, args.loss, args.lr_start)) if args.local_rank == 0 else None
     # 训练
-    train_get(args, data_dict, model_dict)
+    train_get(args, data_dict, model_dict, loss)
