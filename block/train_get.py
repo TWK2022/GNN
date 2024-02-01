27eb56e3dd084ca7ded97562d8ae7a5fa87af80d
TWK <109967047+TWK2022@users.noreply.github.com>
2024-02-01 15:21:04 +0800

    !

--- block/train_get.py
+++ block/train_get.py
@@ -1,56 +1,61 @@
 import tqdm
 import torch
-from block.ModelEMA import ModelEMA
+from block.val_get import val_get
+from block.model_ema import model_ema
 from block.lr_get import adam, lr_adjust
 
 
-def train_get(args, data_dict, model_dict):
+def train_get(args, data_dict, model_dict, loss):
     # 加载模型
     model = model_dict['model'].to(args.device, non_blocking=args.latch)
     # 学习率
     optimizer = adam(args.regularization, args.r_value, model.parameters(), lr=args.lr_start, betas=(0.937, 0.999))
     optimizer.load_state_dict(model_dict['optimizer_state_dict']) if model_dict['optimizer_state_dict'] else None
-    optimizer_adjust = lr_adjust(args, model_dict['lr_adjust_item'])  # 学习率调整函数
-    optimizer = optimizer_adjust(optimizer, model_dict['epoch'] + 1, 0)  # 初始化学习率
+    step_epoch = len(data_dict['train']) // args.batch // args.device_number * args.device_number  # 每轮的步数
+    optimizer_adjust = lr_adjust(args, step_epoch, model_dict['epoch_finished'])  # 学习率调整函数
+    optimizer = optimizer_adjust(optimizer)  # 学习率初始化
     # 使用平均指数移动(EMA)调整参数(不能将ema放到args中，否则会导致模型保存出错)
-    ema = ModelEMA(model) if args.ema else None
+    ema = model_ema(model) if args.ema else None
     if args.ema:
         ema.updates = model_dict['ema_updates']
     # 数据集
-    train_dataset = torch_dataset(args, data_dict['train_input'], model_dict['tokenizer'])
+    train_dataset = torch_dataset(args, data_dict['train'])
     train_shuffle = False if args.distributed else True  # 分布式设置sampler后shuffle要为False
     train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset) if args.distributed else None
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch, shuffle=train_shuffle,
                                                    drop_last=True, pin_memory=args.latch, num_workers=args.num_worker,
                                                    sampler=train_sampler, collate_fn=train_dataset.collate_fn)
+    val_dataset = torch_dataset(args, data_dict['val'])
+    val_sampler = None  # 分布式时数据合在主GPU上进行验证
+    val_batch = args.batch // args.device_number  # 分布式验证时batch要减少为一个GPU的量
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=val_batch, shuffle=False,
+                                                 drop_last=False, pin_memory=args.latch, num_workers=args.num_worker,
+                                                 sampler=val_sampler, collate_fn=train_dataset.collate_fn)
     # 分布式初始化
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank],
                                                       output_device=args.local_rank) if args.distributed else model
-    epoch_base = model_dict['epoch'] + 1  # 新的一轮要+1
-    for epoch in range(epoch_base, epoch_base + args.epoch):
-        # 训练
+    epoch_base = model_dict['epoch_finished'] + 1  # 新的一轮要+1
+    for epoch in range(epoch_base, args.epoch + 1):  # 训练
         print(f'\n-----------------------第{epoch}轮-----------------------') if args.local_rank == 0 else None
         model.train()
-        train_loss = 0  # 记录训练损失
-        tqdm_show = tqdm.tqdm(
-            total=len(data_dict['train_input']) // args.batch // args.device_number * args.device_number, postfix=dict,
-            mininterval=0.2) if args.local_rank == 0 else None  # tqdm
-        for index, (input_ids_batch, attention_mask_batch, label_batch) in enumerate(train_dataloader):
-            input_ids_batch = input_ids_batch.to(args.device, non_blocking=args.latch)
-            attention_mask_batch = attention_mask_batch.to(args.device, non_blocking=args.latch)
-            label_batch = label_batch.to(args.device, non_blocking=args.latch)
+        train_loss = 0  # 记录损失
+        if args.local_rank == 0:  # tqdm
+            tqdm_show = tqdm.tqdm(total=step_epoch, mininterval=0.2)
+        for index, (input_data_batch, edge_index, true_batch) in enumerate(train_dataloader):
+            input_data_batch = input_data_batch.to(args.device, non_blocking=args.latch)
+            edge_index = edge_index.to(args.device, non_blocking=args.latch)
+            true_batch = true_batch.to(args.device, non_blocking=args.latch)
             if args.amp:
                 with torch.cuda.amp.autocast():
-                    pred_batch = model(input_ids=input_ids_batch, attention_mask=attention_mask_batch,
-                                       labels=label_batch)
-                    loss_batch = pred_batch.loss  # 当传入labels时模型内部会自动计算损失
+                    pred_batch = model(input_data_batch, edge_index)
+                    loss_batch = loss(pred_batch, true_batch)
                 args.amp.scale(loss_batch).backward()
                 args.amp.step(optimizer)
                 args.amp.update()
                 optimizer.zero_grad()
             else:
-                pred_batch = model(input_ids=input_ids_batch, attention_mask=attention_mask_batch, labels=label_batch)
-                loss_batch = pred_batch.loss  # 当传入labels时模型内部会自动计算损失
+                pred_batch = model(input_data_batch, edge_index)
+                loss_batch = loss(pred_batch, true_batch)
                 loss_batch.backward()
                 optimizer.step()
                 optimizer.zero_grad()
@@ -58,74 +63,73 @@ def train_get(args, data_dict, model_dict):
             ema.update(model) if args.ema else None
             # 记录损失
             train_loss += loss_batch.item()
+            # 调整学习率
+            optimizer = optimizer_adjust(optimizer)
             # tqdm
             if args.local_rank == 0:
-                tqdm_show.set_postfix({'当前loss': loss_batch.item()})  # 添加loss显示
+                tqdm_show.set_postfix({'train_loss': loss_batch.item(),
+                                       'lr': optimizer.param_groups[0]['lr']})  # 添加显示
                 tqdm_show.update(args.device_number)  # 更新进度条
         # tqdm
-        tqdm_show.close() if args.local_rank == 0 else None
+        if args.local_rank == 0:
+            tqdm_show.close()
         # 计算平均损失
-        train_loss = train_loss / (index + 1)
-        print('\n| train_loss:{:.4f} | lr:{:.6f} |\n'.format(train_loss, optimizer.param_groups[0]['lr']))
-        # 调整学习率
-        optimizer = optimizer_adjust(optimizer, epoch + 1, train_loss)
+        train_loss /= index + 1
+        if args.local_rank == 0:
+            print(f'\n| 训练 | train_loss:{train_loss:.4f} | lr:{optimizer.param_groups[0]["lr"]:.6f} |\n')
         # 清理显存空间
-        del input_ids_batch, attention_mask_batch, label_batch, pred_batch, loss_batch
+        del input_data_batch, edge_index, true_batch, pred_batch, loss_batch
         torch.cuda.empty_cache()
+        # 验证
+        if args.local_rank == 0:  # 分布式时只验证一次
+            val_loss, mae, mse = val_get(args, val_dataloader, model, loss, data_dict, ema)
         # 保存
         if args.local_rank == 0:  # 分布式时只保存一次
             model_dict['model'] = model.module if args.distributed else model
-            model_dict['epoch'] = epoch
+            model_dict['epoch_finished'] = epoch
             model_dict['optimizer_state_dict'] = optimizer.state_dict()
-            model_dict['lr_adjust_item'] = optimizer_adjust.lr_adjust_item
             model_dict['ema_updates'] = ema.updates if args.ema else model_dict['ema_updates']
-            if epoch % 1 == 0:  # 保存peft模型
-                save_name = f'save_peft_{epoch}'
-                model_dict['model'].save_pretrained(save_name)
-                model_dict['tokenizer'].save_pretrained(save_name)
-            if args.save_pt != 0 and epoch % args.save_pt == 0:  # 保存完整模型
-                torch.save(model_dict, 'last.pt')
+            model_dict['train_loss'] = train_loss
+            model_dict['val_loss'] = val_loss
+            model_dict['val_mae'] = mae
+            model_dict['val_mse'] = mse
+            torch.save(model_dict, 'last.pt')  # 保存最后一次训练的模型
+            if mse < 1 and mse < model_dict['standard']:
+                model_dict['standard'] = mse
+                torch.save(model_dict, args.save_path)  # 保存最佳模型
+                print(f'\n| 保存最佳模型:{args.save_path} | val_loss:{val_loss:.4f} | val_mae:{mae:.4f} |'
+                      f' val_mse:{mse:.4f} |\n')
             # wandb
             if args.wandb:
-                args.wandb_run.log({'metric/train_loss': train_loss})
+                args.wandb_run.log({'metric/train_loss': train_loss,
+                                    'metric/val_loss': val_loss,
+                                    'metric/val_mae': mae,
+                                    'metric/val_mse': mse})
         torch.distributed.barrier() if args.distributed else None  # 分布式时每轮训练后让所有GPU进行同步，快的GPU会在此等待
 
 
 class torch_dataset(torch.utils.data.Dataset):
-    def __init__(self, args, input_data, tokenizer):
-        self.input_data = input_data
-        self.tokenizer = tokenizer
-        self.eos_token = tokenizer.eos_token
-        self.pad_token_id = tokenizer.pad_token_id
-        self.prompt = 'You are a helpful assistant. 你是一个乐于助人的助手。'  # 提示词
-        self.template = ('[INST] <<SYS>>\n'
-                         '{prompt}\n'
-                         '<</SYS>>\n\n'
-                         '{instruction} [/INST]')  # 对话模型的输入需要经过特殊的处理
+    def __init__(self, args, data):
+        self.data = data
+        self.input_size = args.input_size
+        self.output_size = args.output_size
+        self.label_index = args.label_index
 
     def __len__(self):
-        return len(self.input_data)
+        return len(self.data)
 
     def __getitem__(self, index):
-        sft_dict = self.input_data[index]
-        instruction = sft_dict['instruction'] + '\n' + sft_dict['input'] if sft_dict['input'] \
-            else sft_dict['instruction']
-        output = sft_dict['output'] + str(self.eos_token)
-        text_merge = self.template.format(prompt=self.prompt, instruction=instruction)  # 对话时的完整输入
-        input_encode = self.tokenizer.encode(text_merge, add_special_tokens=True, return_tensors='pt').squeeze(0)
-        output_encode = self.tokenizer.encode(output, add_special_tokens=False, return_tensors='pt').squeeze(0)
-        input_ids = torch.concat([input_encode, output_encode], dim=0)  # 训练时的完整输入input_ids
-        attention_mask = torch.full_like(input_ids, 1)  # attention_mask
-        label = torch.full_like(input_ids, -100)  # 不需要的地方变为-100
-        label[len(input_encode):] = output_encode  # 训练时的完整标签label
-        return input_ids, attention_mask, label
+        data = self.data[index]
+        input_data = data.x
+        edge_index = data.edge_index
+        true = data.y[:, self.label_index]
+        return input_data, edge_index, true
 
-    def collate_fn(self, getitem_list):  # 自定义__getitem__的合并方式，填充数据然后合并为批量
-        input_ids_list = [_[0] for _ in getitem_list]
-        attention_mask_list = [_[1] for _ in getitem_list]
-        label_list = [_[2] for _ in getitem_list]
-        input_ids_batch = torch.nn.utils.rnn.pad_sequence(input_ids_list, batch_first=True,
-                                                          padding_value=self.pad_token_id)
-        attention_mask_batch = torch.nn.utils.rnn.pad_sequence(attention_mask_list, batch_first=True, padding_value=0)
-        label_batch = torch.nn.utils.rnn.pad_sequence(label_list, batch_first=True, padding_value=-100)
-        return input_ids_batch, attention_mask_batch, label_batch
+    def collate_fn(self, getitem_list):  # 自定义__getitem__的合并方式
+        input_data_list = [_[0] for _ in getitem_list]
+        edge_index_list = [_[1] for _ in getitem_list]
+        true_list = [_[2] for _ in getitem_list]
+        input_data_batch = torch.concat(input_data_list, dim=0)
+        edge_index = edge_index_list[0]
+        true_batch = torch.concat(true_list, dim=0)
+        return input_data_batch, edge_index, true_batch
