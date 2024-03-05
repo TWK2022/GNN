36103499a6d79446ca7baa9923e00d9903f37e3a
TWK <109967047+TWK2022@users.noreply.github.com>
2024-03-05 18:59:22 +0800

    !

--- block/data_get.py
+++ block/data_get.py
@@ -6,17 +6,15 @@ def data_get(args):
     return data_dict
 
 
-class data_prepare(object):
+class data_prepare:
     def __init__(self, args):
         self.data_path = args.data_path
         self.divide = args.divide
 
     def load(self):
-        # 读取数据
         data = torch.load(self.data_path)  # torch_geometric.data.Data格式
-        # 划分数据集
         data_len = len(data)  # 数据总数
-        boundary = int(data_len * self.divide[0] / (self.divide[0] + self.divide[1]))  # 数据划分
+        boundary = int(data_len * self.divide[0] / (self.divide[0] + self .divide[1]))  # 数据划分
         train = data[0:boundary]  # 训练数据
         val = data[boundary:]  # 验证数据
         data_dict = {'train': train, 'val': val}
