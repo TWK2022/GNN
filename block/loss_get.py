4351f9e575fb3a1cab1fb2409ff888a090aedd14
TWK <109967047+TWK2022@users.noreply.github.com>
2024-03-08 10:21:04 +0800

    !

--- block/loss_get.py
+++ block/loss_get.py
@@ -3,6 +3,7 @@ import torch
 
 def loss_get(args):
     choice_dict = {'mae': 'torch.nn.L1Loss()',
-                   'mse': 'torch.nn.MSELoss()'}
+                   'mse': 'torch.nn.MSELoss()',
+                   'cross': 'torch.nn.CrossEntropyLoss()'}
     loss = eval(choice_dict[args.loss])
     return loss
