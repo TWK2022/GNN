117fd530736fd009794c784bac17de249a632fde
TWK <109967047+TWK2022@users.noreply.github.com>
2024-03-08 11:15:49 +0800

    !

--- block/val_get.py
+++ block/val_get.py
@@ -11,7 +11,7 @@ def val_get(args, val_dataloader, model, loss, data_dict, ema):
         val_loss = 0
         tqdm_len = (len(data_dict['val']) - 1) // (args.batch // args.device_number) + 1
         tqdm_show = tqdm.tqdm(total=tqdm_len, mininterval=0.2)
-        for index, (input_data_batch, edge_index, true_batch) in enumerate(val_dataloader):
+        for index, (input_data_batch, edge_index, true_batch, mask) in enumerate(val_dataloader):
             input_data_batch = input_data_batch.to(args.device, non_blocking=args.latch)
             edge_index = edge_index.to(args.device, non_blocking=args.latch)
             true_batch = true_batch.to(args.device, non_blocking=args.latch)
@@ -27,9 +27,12 @@ def val_get(args, val_dataloader, model, loss, data_dict, ema):
         tqdm_show.close()
         # 计算指标
         val_loss /= (index + 1)
-        pred = torch.stack(pred, dim=0)
-        true = torch.stack(true, dim=0)
-        # 计算总相对指标
-        mae, mse = metric(pred, true)
-        print(f'\n| 验证 | val_loss:{val_loss:.4f} | val_mae:{mae:.4f} | val_mse:{mse:.4f} |')
-    return val_loss, mae.item(), mse.item()
+        if args.loss != 'cross':  # 预测数值
+            pred = torch.stack(pred, dim=0)
+            true = torch.stack(true, dim=0)
+            mae, mse = metric(pred, true)
+            print(f'\n| 验证 | val_loss:{val_loss:.4f} | val_mae:{mae:.4f} | val_mse:{mse:.4f} |')
+        else:  # 分类
+            mae, mse = None, None
+            print(f'\n| 验证 | val_loss:{val_loss:.4f} |')
+    return val_loss, mae, mse
